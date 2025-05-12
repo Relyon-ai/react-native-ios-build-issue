0a29b4844c8b3d7be4a438cdf1dded9fcd5bd1bf
Anastasiya <personalnastya8@gmail.com>
2025-05-12 09:13:42 +0300

    Test app

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,3 @@
 module.exports = {
-  presets: ['babel-preset-expo'],
-  plugins: [
-    ["module:react-native-dotenv", {
-      "envName": "APP_ENV",
-      "moduleName": "@env",
-      "path": ".env",
-      "blocklist": null,
-      "allowlist": null,
-      "safe": false,
-      "allowUndefined": true,
-      "verbose": false,
-    }],
-    'react-native-reanimated/plugin',
-  ],
+  presets: ['module:@react-native/babel-preset'],
 };
--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,4 @@
-const { getDefaultConfig } = require('expo/metro-config');
-const { mergeConfig } = require('@react-native/metro-config');
-
-const defaultConfig = getDefaultConfig(__dirname);
-
-const {
-  resolver: { sourceExts, assetExts },
-} = defaultConfig;
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
 /**
  * Metro configuration
@@ -13,14 +6,6 @@ const {
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {
-  transformer: {
-    babelTransformerPath: require.resolve('react-native-svg-transformer'),
-  },
-  resolver: {
-    assetExts: assetExts.filter((ext) => ext !== 'svg'),
-    sourceExts: [...sourceExts, 'svg'],
-  },
-};
+const config = {};
 
-module.exports = mergeConfig(defaultConfig, config);
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
